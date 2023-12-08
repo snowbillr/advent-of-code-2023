f693252f229d845727c640591729c3792a8c3ba6
Bill Reed <billnreed@gmail.com>
2023-12-08 00:28:32 +0000

    day 4 part 2

--- day_04/part_2.js
+++ day_04/part_2.js
@@ -1,15 +1,22 @@
 const readLines = require('../utils/readLines')
 const Card = require('./utils/card')
 
-const input = readLines(__dirname, 'data/part_1_test_input.txt')
-// const input = readLines(__dirname, 'data/input.txt')
+// const input = readLines(__dirname, 'data/part_1_test_input.txt')
+const input = readLines(__dirname, 'data/input.txt')
 
 const cards = input.map(Card.parse)
-
 // indexed by card number - 1
 const cardCounts = Array.from({ length: cards.length }, () => 1)
+
 cards.forEach((card, i) => {
     const matchingCount = card.countMatches()
-    const copies = cards.slice(i, i + matchingCount)
-    // console.log()
+    for (let j = 1; j <= matchingCount; j++) {
+        const nextIndex = i + j;
+        if (nextIndex < cardCounts.length) {
+            cardCounts[nextIndex] += cardCounts[i]
+        }
+    }
 })
+
+const sum = cardCounts.reduce((a, b) => a + b, 0)
+console.log(sum)
